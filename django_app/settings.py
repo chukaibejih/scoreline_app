f32b00d7f27207daefd3634ad671b5f3c1da6223
Ibejih <chukaibejih@gmail.com>
2021-11-13 14:06:27 +0100

    Footer

--- django_app/settings.py
+++ django_app/settings.py
@@ -132,6 +132,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL ='login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'chukwukaibejih@gmail.com'
+EMAIL_HOST_PASSWORD = '2000money'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
