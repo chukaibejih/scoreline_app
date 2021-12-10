1c1b6432821d3e87b5c0358f3b08ff18bb3fbfda
Ibejih <chukaibejih@gmail.com>
2021-12-10 23:27:46 +0100

    Initial commit

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(validators=[MinLengthValidator(100)])
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE) 
     likes = models.ManyToManyField(User, related_name='blog_posts')
--- blog/urls.py
+++ blog/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
     path('post/<int:pk>/comment/', CommentCreateView.as_view(), name='comment-create'),
-    path('like/<int:pk>', LikeView, name='like_post'),
+    path('like/<int:pk>', LikeView, name='like-post'),
     
 ]
